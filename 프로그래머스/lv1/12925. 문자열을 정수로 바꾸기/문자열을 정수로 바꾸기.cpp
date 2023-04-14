871b158db44d8f99074e14add5d2234b77d6d516
Ma <58978349+richard7714@users.noreply.github.com>
2023-04-14 20:07:06 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -6,31 +6,31 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = 0;
-    unordered_map <char,int> s2n = {{'0',0},{'1',1},{'2',2},{'3',3},{'4',4},{'5',5},{'6',6},{'7',7},{'8',8},{'9',9}};
+    int answer = stoi(s);
+//     unordered_map <char,int> s2n = {{'0',0},{'1',1},{'2',2},{'3',3},{'4',4},{'5',5},{'6',6},{'7',7},{'8',8},{'9',9}};
     
-    int index = s.size();
-    bool isMinus;
+//     int index = s.size();
+//     bool isMinus;
     
-    for (char c : s)
-    {   
-        if(s2n.find(c) != s2n.end())
-        {
-            answer += s2n[c] * pow(10,(index-1));
-        }
-        else
-        {
-            if(c=='-')
-                isMinus = true;
-            else
-                isMinus = false;
-        }
+//     for (char c : s)
+//     {   
+//         if(s2n.find(c) != s2n.end())
+//         {
+//             answer += s2n[c] * pow(10,(index-1));
+//         }
+//         else
+//         {
+//             if(c=='-')
+//                 isMinus = true;
+//             else
+//                 isMinus = false;
+//         }
         
-        index--;
-    }
+//         index--;
+//     }
     
-    if(isMinus)
-        answer = -answer;
-    
+//     if(isMinus)
+//         answer = -answer;
+
     return answer;
 }
