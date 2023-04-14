c3cc041ba22cbab4c97d3691eb391b53b92459ad
Ma <58978349+richard7714@users.noreply.github.com>
2023-04-14 13:59:04 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    unordered_set <int> candid;
+    
+    if(!n)
+        return answer;
+    
+    for (auto idx = 1; idx < n+1;idx++)
+    {
+        if(!(n%idx))
+        {
+            if(candid.find(idx) == candid.end())
+            {
+                candid.insert(idx);
+                candid.insert(n / idx);
+            }
+        }
+    }
+    
+    for (auto idx = candid.begin();idx != candid.end();idx++)
+    {
+        answer += *idx;
+    }
+    
+    return answer;
+}
