17421d69d04d4ec0417d72ab751b26ac98884eed
Ma <58978349+richard7714@users.noreply.github.com>
2023-04-29 14:27:54 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int A,B,C;
+
+    std::cin>>A>>B>>C;
+
+    std::cout<<(A+B)%C<<std::endl;
+    std::cout<<((A%C)+(B%C))%C<<std::endl;
+    std::cout<<(A*B)%C<<std::endl;
+    std::cout<<((A%C)*(B%C))%C<<std::endl;
+
+    return 0;
+
+}
