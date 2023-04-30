6652127025868aaa1a4913f39bb8cb0bff694452
Ma <58978349+richard7714@users.noreply.github.com>
2023-04-30 14:29:22 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int A;
+    std::cin>>A;
+
+    std::cout<<A<<" * "<<"1 = "<<A*1<<std::endl;
+    std::cout<<A<<" * "<<"2 = "<<A*2<<std::endl;
+    std::cout<<A<<" * "<<"3 = "<<A*3<<std::endl;
+    std::cout<<A<<" * "<<"4 = "<<A*4<<std::endl;
+    std::cout<<A<<" * "<<"5 = "<<A*5<<std::endl;
+    std::cout<<A<<" * "<<"6 = "<<A*6<<std::endl;
+    std::cout<<A<<" * "<<"7 = "<<A*7<<std::endl;
+    std::cout<<A<<" * "<<"8 = "<<A*8<<std::endl;
+    std::cout<<A<<" * "<<"9 = "<<A*9<<std::endl;
+
+}
