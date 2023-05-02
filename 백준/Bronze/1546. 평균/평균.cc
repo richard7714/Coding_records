faac83e42645f67ae16f600104e93e3aab4ab97c
Ma <58978349+richard7714@users.noreply.github.com>
2023-05-02 16:16:25 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1228 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <algorithm>
-#include <set>
 #include <vector>
 #include <numeric>
 
@@ -20,18 +19,15 @@ int main() {
 
     auto iter = scores.begin();
     float M = *std::max_element(scores.begin(),scores.end());
-    float mean=0;
 
     while(iter!=scores.end())
     {
         *iter = *iter/M * 100;
-        mean+= *iter;
         iter++;
     }
 
-//    double mean = std::accumulate(scores.begin(),scores.end(),double(0));
-//    mean /= scores.size();
-    mean/= scores.size();
+    double mean = std::accumulate(scores.begin(),scores.end(),float(0));
+    mean /= scores.size();
     printf("%f",mean);
 
 }
