cdd07b2a12e9618e0ce246de76835736b20c8a81
Ma <58978349+richard7714@users.noreply.github.com>
2023-05-10 13:36:16 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,46 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-vector <int> dw;
-int height, sum;
-
-int main() {
+int a[9];
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-    for (int i = 0; i < 9; i++) {
-        cin >> height;
-        dw.push_back(height);
+    for(int i = 0; i<9; i++){
+        cin >> a[i];
     }
-
-    sort(dw.begin(), dw.end());
-
-    vector<int> idx(7, 1);
-    idx.push_back(0);
-    idx.push_back(0);
-    sort(idx.begin(), idx.end());
-
-    for (int i = 0; i < 36; i++) {
-        auto it = idx.begin();
-        while (it != idx.end()) {
-            if (*it)
-                sum += dw[it - idx.begin()];
-            it++;
-        }
-        
-        if (sum != 100) {
-            sum = 0;
-            next_permutation(idx.begin(), idx.end());
-            continue;
-        }
-        else {
-            auto it2 = dw.begin();
-            for (int idx2: idx) {
-                if (idx2)
-                    cout << *it2 << '\n';
-                it2++;
-            }
-            exit(0);
-        }
-    }
+    sort(a, a+9);
+    do{
+        int sum = 0;
+        for(int i = 0; i< 7; i++) sum += a[i];
+        if(sum == 100) break;
+    }while(next_permutation(a,a+9));
+    for(int i = 0; i<7; i++) cout << a[i] << "\n";
+    return 0;
 }
