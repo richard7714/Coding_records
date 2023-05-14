fdfbecc0d7739122c57d4ffc4eb43ce42666cebe
Ma <58978349+richard7714@users.noreply.github.com>
2023-05-14 15:21:11 +0900

    [Silver III] Title: 수열, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,43 +1,21 @@
+// 제공된 정답
 #include <bits/stdc++.h>
 using namespace std;
-
-int N;
-int K;
-int sum;
-
-int main()
-{
-    ios::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
-    cin >> N;
-    cin >> K;
-
-    int temp[N];
-
-    for(int i = 0; i<N;i++){
-        cin >> temp[i];
+typedef long long ll;
+int n,k,temp,psum[100001], ret = -1e9;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++){
+        // 누적합을 기록하여 더하는 과정 생략
+        cin >> temp; psum[i] = psum[i-1] + temp;
     }
-
-    auto idx = temp;
-    int sum_temp = 0;
-
-    while(idx != temp+N-K+1)
-    {
-        if(idx == temp) {
-            sum = accumulate(idx, idx + K, 0);
-            sum_temp = sum;
-        }
-
-        else
-        {
-            sum_temp = sum_temp - *(idx-1) + *(idx+K-1);
-            if(sum < sum_temp)
-                sum = sum_temp;
-        }
-        idx++;
-//        cout<<"sum : "<<sum<<" sum_temp : "<< sum_temp <<"\n";
+    for(int i = k; i<=n; i++){
+        // 누적합사이의 차를 구해 원하는 범위의 누적합을 획득
+        // max함수를 통해 대소 비교
+        ret = max(ret, psum[i]-psum[i-k]);
     }
-
-
-    cout << sum;
-
+    cout << ret << "\n";
+    return 0;
 }
