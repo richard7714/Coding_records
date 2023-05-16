a20e1c80dab32bbe8ff065b561ca268e54644740
Ma <58978349+richard7714@users.noreply.github.com>
2023-05-16 15:39:32 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,50 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int T,N;
-string wear,wear_class;
-
+int t,n;
+string a,b;
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> T;
-    int ret[T];
-
-    for (int i =0; i<T; i++)
-    {
-        cin >> N;
-        ret[i] = 0;
-        map<string,int> wear_list;
-
-        if(N == 0) {
-            continue;
+    cin >> t;
+    while(t--){
+        map<string, int> _map;
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            cin >> a >> b;
+            _map[b]++;
         }
+        long long ret = 1;
 
-        for (int j = 0; j<N; j++)
-        {
-            cin >> wear >> wear_class;
-            if(wear_list.find(wear_class) == wear_list.end())
-                wear_list.insert({wear_class, 1});
-            else
-                wear_list[wear_class]++;
+        // map도 : 형태로 접근 가능하다
+        for(auto c : _map){
+            ret *= ((long long)c.second+1);
         }
-
-        for(auto it = wear_list.begin(); it!=wear_list.end();it++)
-        {
-            if(it==wear_list.begin())
-                ret[i] = (*it).second + 1;
-            else
-                ret[i] *= ((*it).second+1);
-
-        }
-
-        ret[i] -= 1;
-
+        ret--;
+        cout << ret << "\n";
     }
-
-    for(int out : ret)
-        cout << out << "\n";
-
+    return 0;
 }
